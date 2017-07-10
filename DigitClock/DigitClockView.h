afdd19d43df02eaec4f5831e666fcd7181d3dfc6
文优 <wenyou.ly@taobao.com>
2017-07-10 18:35:17 +0800

    update

---
+++ DigitClock/DigitClockView.h
@@ -0,0 +1,13 @@
+//
+//  DigitClockView.h
+//  DigitClock
+//
+//  Created by wenyou on 2017/7/10.
+//  Copyright © 2017年 wenyou. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface DigitClockView : ScreenSaverView
+
+@end
---
+++ DigitClock/Font.h
@@ -0,0 +1,21 @@
+//
+//  Font.h
+//  DigitClock
+//
+//  Created by wenyou on 2017/7/10.
+//  Copyright © 2017年 wenyou. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+typedef NS_ENUM(NSInteger, FontName) {
+    DigitalDismay,
+    DSDigi
+
+
+//    Digital Dismay": @"otf", @"DS-DIGI
+};
+
+@interface Font : NSObject
+
+@end
